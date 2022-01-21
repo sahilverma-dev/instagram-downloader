27e4c2749249f80de19455eacbb011132acf976b
Sahil Verma <domainsahil@gmail.com>
2022-01-21 21:31:03 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,6 @@ app.listen(PORT, () => {
 
 app.use(cors());
 
-// change
 const getBase64 = async (link) => {
   const base64 = await imageToBase64(link);
   return `data:image/jpeg;base64,${base64}`;
@@ -29,7 +28,8 @@ app.get("/post/:id", (req, res) => {
     const link = `https://www.instagram.com/p/${id}/?__a=1`;
     request(link, (err, response, html) => {
       if (!err) {
-        const json = JSON.parse(html)?.graphql?.shortcode_media;
+        const json = JSON?.parse(html)?.graphql?.shortcode_media;
+        if (!json) console.log("tu sahi thi");
         const promiseArray = json?.edge_sidecar_to_children?.edges?.map(
           async (item) => ({
             displayUrl: await getBase64(item?.node?.display_url),
@@ -81,47 +81,50 @@ app.get("/user/:username", (req, res) => {
     const link = `https://www.instagram.com/${username}/?__a=1`;
     request(link, (err, response, html) => {
       if (!err) {
-        const json = JSON.parse(html)?.graphql?.user;
-        const promiseArray = json?.edge_owner_to_timeline_media?.edges?.map(
-          async (item) => ({
-            displayUrl: await getBase64(item?.node?.display_url),
-            id: item?.node?.shortcode,
-            location: item?.node?.location,
-            caption: {
-              main: item?.node?.edge_media_to_caption.edges[0].node.text,
-              accessibile: item?.node?.accessibility_caption,
-            },
-            comments: item?.node?.edge_media_to_comment.count,
-            isVideo: item?.node?.is_video,
-            likes: item?.node?.edge_liked_by.count,
-            isCollection: item?.node?.edge_sidecar_to_children ? true : false,
-            resolutions: item?.node?.thumbnail_resources,
-          })
-        );
-        let allImages;
-        if (promiseArray) allImages = Promise.all(promiseArray);
-        if (json)
-          (async () => {
-            res?.send({
-              username,
-              bio: json.biography,
-              isVerified: json?.is_verified,
-              category: json?.category_name,
-              externalURL: json?.external_url,
-              profilePic: {
-                sd: await getBase64(json?.profile_pic_url),
-                hd: await getBase64(json?.profile_pic_url_hd),
+        if (html) {
+          const json = JSON?.parse(html)?.graphql?.user;
+          if (!json) console.log("tu sahi thi");
+          const promiseArray = json?.edge_owner_to_timeline_media?.edges?.map(
+            async (item) => ({
+              displayUrl: await getBase64(item?.node?.display_url),
+              id: item?.node?.shortcode,
+              location: item?.node?.location,
+              caption: {
+                main: item?.node?.edge_media_to_caption.edges[0]?.node.text,
+                accessibile: item?.node?.accessibility_caption,
               },
-              fullName: json?.full_name,
-              following: json?.edge_followed_by.count,
-              follows: json?.edge_follow.count,
-              posts: {
-                total: json?.edge_owner_to_timeline_media?.count,
-                content: await allImages,
-              },
-            });
-          })();
-        if (!json) res?.status(400).send("ERROR");
+              comments: item?.node?.edge_media_to_comment.count,
+              isVideo: item?.node?.is_video,
+              likes: item?.node?.edge_liked_by.count,
+              isCollection: item?.node?.edge_sidecar_to_children ? true : false,
+              resolutions: item?.node?.thumbnail_resources,
+            })
+          );
+          let allImages;
+          if (promiseArray) allImages = Promise.all(promiseArray);
+          if (json)
+            (async () => {
+              res?.send({
+                username,
+                bio: json.biography,
+                isVerified: json?.is_verified,
+                category: json?.category_name,
+                externalURL: json?.external_url,
+                profilePic: {
+                  sd: await getBase64(json?.profile_pic_url),
+                  hd: await getBase64(json?.profile_pic_url_hd),
+                },
+                fullName: json?.full_name,
+                following: json?.edge_followed_by.count,
+                follows: json?.edge_follow.count,
+                posts: {
+                  total: json?.edge_owner_to_timeline_media?.count,
+                  content: await allImages,
+                },
+              });
+            })();
+          if (!json) res?.status(400).send("ERROR");
+        }
       }
     });
   }
